f5de2942f776df45dff56a632edcb9d4dd0ce382
juanf.b <juanfbustamante94@gmail.com>
2021-10-29 18:15:56 -0300

    final commit

--- scripts.js
+++ scripts.js
@@ -1,20 +1,28 @@
+/* navbar effect */
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.getElementById("navbar").style.backgroundColor = "#1687a7";
+    document.getElementById("navbar").style.backgroundColor = "#c74f09";
     document.getElementById("navbar").style.padding = "1.20vh 0";
-    document.getElementById("navbar").style.transition = "all 250ms";
-    document.getElementById("logo").style.width = "4.68vw"
+    document.getElementById("logo").style.width = "4.68vw";
     document.getElementById("logo").style.height = "9.02vh";
+    // document.getElementById("logo").style.display = "block"
+    document.getElementById("navbar").style.transition = "all 250ms";
+    
   } else {
     document.getElementById("navbar").style.backgroundColor = "transparent";
     document.getElementById("navbar").style.padding = "1.35vh 0";
+    
     document.getElementById("logo").style.width = "5.07vw"
     document.getElementById("logo").style.height = "10.65vh";
+    // document.getElementById("logo").style.display = "none";
+    
   }
 }
 
+
+/* Scroll smooth */
 function scrollTo(){
   document.querySelectorAll(".scroll").forEach(t=>t.onclick=scrollAnchors)}
   function scrollAnchors(t,o=null){
